16bf792ec67141b5adef15faa40a0dbe22cc0f85
Alex <aconstantin86@yahoo.com>
2023-02-25 14:35:07 +0200

    Finished hw Clean Code

--- Exercises/yahtzee/Yahtzee.cs
+++ Exercises/yahtzee/Yahtzee.cs
@@ -1,238 +1,138 @@
 public class Yahtzee {
 
-    public static int Chance(int d1, int d2, int d3, int d4, int d5)
+    public static void CheckDice(params int[] dice)
     {
-        int total = 0;
-        total += d1;
-        total += d2;
-        total += d3;
-        total += d4;
-        total += d5;
-        return total;
+        if (dice.Length != 5)
+            throw new ArgumentException("dice must have 5 elements", "dice");
+        if (dice.Any(d => d < 1 || d > 6))
+            throw new ArgumentException("dice must have values between 1 and 6", "dice");
+    }
+
+    public static int Chance(params int[] dice)
+    {
+        CheckDice(dice);
+        return dice.Sum();
     }
 
     public static int yahtzee(params int[] dice)
     {
-        int[] counts = new int[6];
-        foreach (int die in dice)
-            counts[die-1]++;
-        for (int i = 0; i != 6; i++)
-            if (counts[i] == 5)
-                return 50;
+        CheckDice(dice);
+        if (dice.Distinct().Count() == 1)
+            return 50;
         return 0;
     }
 
-    public static int Ones(int d1, int d2, int d3, int d4, int d5) {
-        int sum = 0;
-        if (d1 == 1) sum++;
-        if (d2 == 1) sum++;
-        if (d3 == 1) sum++;
-        if (d4 == 1) sum++;
-        if (d5 == 1)
-            sum++;
-
-        return sum;
+    public static int AllKindOfScores(int[] dice, int kind)
+    {
+        CheckDice(dice);
+        int total=0;
+        total+= dice.Where(d => d == kind).Sum();
+        return total;
     }
 
-    public static int Twos(int d1, int d2, int d3, int d4, int d5) {
-        int sum = 0;
-        if (d1 == 2) sum += 2;
-        if (d2 == 2) sum += 2;
-        if (d3 == 2) sum += 2;
-        if (d4 == 2) sum += 2;
-        if (d5 == 2) sum += 2;
-        return sum;
+    public static int Ones(params int[] dice) {
+        return AllKindOfScores(dice, 1);
     }
 
-    public static int Threes(int d1, int d2, int d3, int d4, int d5) {
-        int s;
-        s = 0;
-        if (d1 == 3) s += 3;
-        if (d2 == 3) s += 3;
-        if (d3 == 3) s += 3;
-        if (d4 == 3) s += 3;
-        if (d5 == 3) s += 3;
-        return s;
+    public static int Twos(params int[] dice) {
+        return AllKindOfScores(dice, 2);
+    }
+
+    public static int Threes(params int[] dice) {
+        return AllKindOfScores(dice, 3);
     }
 
     protected int[] dice;
-    public Yahtzee(int d1, int d2, int d3, int d4, int _5)
+    public Yahtzee(params int[] diceGiven)
     {
-        dice = new int[5];
-        dice[0] = d1;
-        dice[1] = d2;
-        dice[2] = d3;
-        dice[3] = d4;
-        dice[4] = _5;
+        CheckDice(diceGiven);
+        dice = diceGiven;
+        
     }
 
     public int Fours()
     {
-        int sum;
-        sum = 0;
-        for (int at = 0; at != 5; at++) {
-            if (dice[at] == 4) {
-                sum += 4;
-            }
-        }
-        return sum;
+        return AllKindOfScores(dice, 4);
     }
 
     public int Fives()
     {
-        int s = 0;
-        int i;
-        for (i = 0; i < dice.Length; i++)
-            if (dice[i] == 5)
-                s = s + 5;
-        return s;
+        return AllKindOfScores(dice, 5);
     }
 
     public int sixes()
     {
-        int sum = 0;
-        for (int at = 0; at < dice.Length; at++)
-            if (dice[at] == 6)
-                sum = sum + 6;
-        return sum;
+        return AllKindOfScores(dice, 6);
     }
 
-    public static int ScorePair(int d1, int d2, int d3, int d4, int d5)
+    public static int ScorePair(params int[] dice)
     {
-        int[] counts = new int[6];
-        counts[d1-1]++;
-        counts[d2-1]++;
-        counts[d3-1]++;
-        counts[d4-1]++;
-        counts[d5-1]++;
-        int at;
-        for (at = 0; at != 6; at++)
-            if (counts[6-at-1] == 2)
-                return (6-at)*2;
+        CheckDice(dice);
+        var score = dice.GroupBy(d => d).Where(d => d.Count() == 2).Select(d => d.Key*2);
+        return score.Max();
+    }
+
+    public static int TwoPair(params int[] dice)
+    {
+        CheckDice(dice);
+
+        var score = dice.GroupBy(d => d).Where(d => d.Count() ==2).Select(d => d.Key * 2);
+        if(score.Count()==2)
+            return score.Sum();
         return 0;
     }
 
-    public static int TwoPair(int d1, int d2, int d3, int d4, int d5)
+    public static int FourOfAKind(params int[] dice)
     {
-        int[] counts = new int[6];
-        counts[d1-1]++;
-        counts[d2-1]++;
-        counts[d3-1]++;
-        counts[d4-1]++;
-        counts[d5-1]++;
-        int n = 0;
-        int score = 0;
-        for (int i = 0; i < 6; i += 1)
-            if (counts[6-i-1] == 2) {
-                n++;
-                score += (6-i);
-            }
-        if (n == 2)
-            return score * 2;
-        else
-            return 0;
+        CheckDice(dice);
+
+        return dice.GroupBy(d => d).Where(d => d.Count() ==4).Select(d => d.Key * 4).Sum();
     }
 
-    public static int FourOfAKind(int _1, int _2, int d3, int d4, int d5)
+    public static int ThreeOfAKind(params int[] dice)
     {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[_1-1]++;
-        tallies[_2-1]++;
-        tallies[d3-1]++;
-        tallies[d4-1]++;
-        tallies[d5-1]++;
-        for (int i = 0; i < 6; i++)
-            if (tallies[i] == 4)
-                return (i+1) * 4;
-        return 0;
+        CheckDice(dice);
+
+        return dice.GroupBy(d => d).Where(d => d.Count() == 3).Select(d => d.Key * 3).Sum();
     }
 
-    public static int ThreeOfAKind(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] t;
-        t = new int[6];
-        t[d1-1]++;
-        t[d2-1]++;
-        t[d3-1]++;
-        t[d4-1]++;
-        t[d5-1]++;
-        for (int i = 0; i < 6; i++)
-            if (t[i] == 3)
-                return (i+1) * 3;
-        return 0;
-    }
+    
 
-    public static int SmallStraight(int d1, int d2, int d3, int d4, int d5)
+
+    public static int SmallStraight(params int[] dice)
     {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[d1-1] += 1;
-        tallies[d2-1] += 1;
-        tallies[d3-1] += 1;
-        tallies[d4-1] += 1;
-        tallies[d5-1] += 1;
-        if (tallies[0] == 1 &&
-            tallies[1] == 1 &&
-            tallies[2] == 1 &&
-            tallies[3] == 1 &&
-            tallies[4] == 1)
+        CheckDice(dice);
+        int[] correctSmallStraightInts = { 1, 2, 3, 4, 5 };
+
+        dice= dice.OrderBy(d => d).ToArray();
+        if (dice.SequenceEqual(correctSmallStraightInts))
             return 15;
         return 0;
+        
+
     }
 
-    public static int LargeStraight(int d1, int d2, int d3, int d4, int d5)
+    public static int LargeStraight(params int[] dice)
     {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[d1-1] += 1;
-        tallies[d2-1] += 1;
-        tallies[d3-1] += 1;
-        tallies[d4-1] += 1;
-        tallies[d5-1] += 1;
-        if (tallies[1] == 1 &&
-            tallies[2] == 1 &&
-            tallies[3] == 1 &&
-            tallies[4] == 1
-            && tallies[5] == 1)
+        CheckDice(dice);
+
+        int[] correctLargeStraightInts = { 2,3,4,5,6 };
+        dice = dice.OrderBy(d => d).ToArray();
+        if (dice.SequenceEqual(correctLargeStraightInts))
             return 20;
         return 0;
     }
 
-    public static int FullHouse(int d1, int d2, int d3, int d4, int d5)
+    public static int FullHouse(params int[] dice)
     {
-        int[] tallies;
-        bool _2 = false;
-        int i;
-        int _2_at = 0;
-        bool _3 = false;
-        int _3_at = 0;
+        CheckDice(dice);
 
+        bool hasTwo = dice.GroupBy(d => d).Where(d => d.Count() == 2).Count() == 1;
+        bool hasThree = dice.GroupBy(d => d).Where(d => d.Count() == 3).Count() == 1;
+        if(hasThree && hasTwo)
+            return dice.Sum();
+        return 0;
 
-
-
-        tallies = new int[6];
-        tallies[d1-1] += 1;
-        tallies[d2-1] += 1;
-        tallies[d3-1] += 1;
-        tallies[d4-1] += 1;
-        tallies[d5-1] += 1;
-
-        for (i = 0; i != 6; i += 1)
-            if (tallies[i] == 2) {
-                _2 = true;
-                _2_at = i+1;
-            }
-
-        for (i = 0; i != 6; i += 1)
-            if (tallies[i] == 3) {
-                _3 = true;
-                _3_at = i+1;
-            }
-
-        if (_2 && _3)
-            return _2_at * 2 + _3_at * 3;
-        else
-            return 0;
+        //trebuie mentionat aici ca hasTwo si hasThree au fost folosite pentru a usura citirea codului, ar fi putut fi doar un if cu ambele conditii
     }
 }
